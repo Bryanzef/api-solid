56365a27dfe60a099e6f8ed57280d2c33ec91985
Bryanzef <bryanzef7@gmail.com>
2023-07-21 08:32:26 -0300

    fix;

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,4 @@
-import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repositoy";
+import { PrismaUsersRepository } from "@/repositories/prisma/prisma-users-repository";
 import { userAlreadyExistsError } from "@/use-cases/errors/user-already-exists-error";
 import { RegisterUseCase } from "@/use-cases/register";
 import { FastifyRequest, FastifyReply } from "fastify";
